6e29278bed4177be5572655127f6de23800d9f7b
Mahesh <jadonmaheshpalsingh@gmail.com>
2022-04-13 18:41:02 +0530

    13

--- src/models/user.js
+++ src/models/user.js
@@ -100,7 +100,9 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, "thisismyapi");
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismyapi", {
+    expiresIn: "30 days",
+  });
   // console.log(token);
   user.tokens = user.tokens.concat({ token });
   await user.save();
